0342708d819dcf8ef876bbbc6e5d07623d339f5d
dsalazar117 <dsalazar@ai27.com>
2024-11-15 01:44:49 -0600

    network issues

--- api/data_access/connector.py
+++ api/data_access/connector.py
@@ -3,7 +3,7 @@ import time
 from data_access.elo_service import EloService
 import os 
 
-mongo_url = mongo_url = os.getenv("MONGO_URL", None)
+mongo_url = "mongodb://mongodb:27017"
 max_retries = 5
 retry_interval = 10  # seconds
 

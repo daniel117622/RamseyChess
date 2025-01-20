cf9153e57c60c5ec7ea0a1f405d814a6358832ae
dsalazar117 <dsalazar@ai27.com>
2025-01-20 00:33:48 -0600

    eventlet added

--- api/server.py
+++ api/server.py
@@ -330,4 +330,9 @@ def handle_ping_socket(message):
 
 
 if __name__ == '__main__':
+    import eventlet
+    import eventlet.wsgi
+    eventlet.monkey_patch()
+
+    # Use the Eventlet server
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)

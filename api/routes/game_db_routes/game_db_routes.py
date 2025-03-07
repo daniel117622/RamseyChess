c78a25520eff27cb3d05182e12c6326295528e77
dsalazar117 <dsalazar@ai27.com>
2025-03-06 22:59:50 -0700

    hotfix

--- api/routes/game_db_routes/game_db_routes.py
+++ api/routes/game_db_routes/game_db_routes.py
@@ -28,41 +28,15 @@ def get_games_by_id():
 
     return jsonify(game_manager.getCurrent()), 200
 
-@game_db_routes.route('/get_game_by_owner', methods=['GET'])
-@post_exception_handler 
-def get_games_by_id_paged():
-    data           = request.args
-    user_id        = data.get("sub")
-    items_per_page = int(data.get("items_per_page", 10))
-    page_number    = int(data.get("page_number", 1))
-
-    if not user_id:
-        return jsonify({"error": "User ID is required"}), 400
-
-    game_manager = ChessGameManager()
-    games        = game_manager.loadByOwner(user_id)
-
-    total_items = len(games)
-    total_pages = math.ceil(total_items / items_per_page)
-    start_idx   = (page_number - 1) * items_per_page
-    end_idx     = start_idx + items_per_page
-    paged_games = games[start_idx:end_idx]
-
-    return jsonify({
-        "total_items": total_items,
-        "total_pages": total_pages,
-        "current_page": page_number,
-        "games": [game_manager.getCurrent() for game in paged_games]
-    }), 200
-
 
 @game_db_routes.route('/get_games_by_owner_paged', methods=['GET'])
 @post_exception_handler
 def get_games_by_owner_paged():
     data = request.args
-    user_id = data.get("sub")
+
+    user_id        = data.get("sub")
     items_per_page = int(data.get("items_per_page", 10))
-    page_number = int(data.get("page_number", 1))
+    page_number    = int(data.get("page_number", 1))
 
     if not user_id:
         return jsonify({"error": "User ID is required"}), 400
@@ -97,7 +71,9 @@ def get_games_by_owner():
     if not games:
         return jsonify({"error": "No games found for this user"}), 404
 
-    return jsonify([game_manager.getCurrent() for game in games]), 200
+    game_manager.loadByOwner(user_id)
+    response = game_manager.getCurrent()
+    return jsonify(response), 200
 
 
 @game_db_routes.route('/post_pvp_game', methods=['POST'])

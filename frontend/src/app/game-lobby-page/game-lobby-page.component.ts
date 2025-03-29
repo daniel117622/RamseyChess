e6de62429cb1eb8fe51740ab87de7ed8c885f95d
daniel117622 <dan5jul@hotmail.com>
2025-03-28 18:48:06 -0700

    used request lobbies function

--- frontend/src/app/game-lobby-page/game-lobby-page.component.ts
+++ frontend/src/app/game-lobby-page/game-lobby-page.component.ts
@@ -107,7 +107,7 @@ export class GameLobbyPageComponent implements OnInit
 
     this.lobby.initializeSocket()
       .then(() => {
-        this.availableLobbies$ = this.lobby.onLobbyStateUpdate();
+        this.availableLobbies$ = this.lobby.requestLobbies()
       })
       .catch(error => {
         console.error('Socket initialization failed:', error);

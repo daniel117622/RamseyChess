5204a768cff9241b94a48adfffac3aefcbd2a132
daniel117622 <dan5jul@hotmail.com>
2025-04-01 12:43:45 -0700

    overlay on copy2

--- frontend/src/app/game-db/game-db.component.ts
+++ frontend/src/app/game-db/game-db.component.ts
@@ -1,71 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
-import { GameService , Game } from 'src/services/game-service.service';
+import { GameService, Game } from 'src/services/game-service.service';
 import { switchMap, filter } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Clipboard } from '@angular/cdk/clipboard';
 import { Router } from '@angular/router';
-
+import { Overlay, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
 
 @Component(
 {
-  selector: 'app-game-db',
-  templateUrl: './game-db.component.html',
-  styleUrls: ['./game-db.component.scss']
+    selector   : 'app-game-db',
+    templateUrl: './game-db.component.html',
+    styleUrls  : ['./game-db.component.scss']
 })
 export class GameDbComponent implements OnInit
 {
-  gameId: string | null = null;
-  game  :   Game | null = null;
+    gameId: string | null = null;
+    game  : Game | null = null;
+    private overlayRef: OverlayRef | null = null;
 
-  constructor(
-    private route      : ActivatedRoute,
-    private gameService: GameService,
-    public  auth       : AuthService,
-    private clipboard  : Clipboard,
-    private router     : Router
-  )
-  {}
+    @ViewChild('copiedMessage') copiedMessage!: TemplateRef<any>;
 
-  ngOnInit(): void
-  {
-    // Retrieve the 'game-id' parameter from the route
-    this.gameId = this.route.snapshot.paramMap.get('id');
-    
-
-    if (this.gameId)
+    constructor(
+        private route        : ActivatedRoute,
+        private gameService  : GameService,
+        public  auth         : AuthService,
+        private clipboard    : Clipboard,
+        private router       : Router,
+        private overlay      : Overlay,
+        private viewContainerRef: ViewContainerRef
+    )
     {
-      this.auth.user$.pipe(
-        filter((user): user is { sub: string } => user != null),  
-        switchMap(user => this.loadGameDetails(this.gameId || '', user.sub))  
-      ).subscribe(
-        (data: Game) => 
-        {
-          this.game = data;
-          console.log('Game details:', this.game);
-        },
-        (error) => 
-        {
-          console.error('Error fetching game details:', error);
-        }
-      );
     }
-  }
 
-  loadGameDetails(gameId: string, sub: string): Observable<Game>
-  {
-    return this.gameService.getGameById(gameId, sub);
-  }
+    ngOnInit(): void
+    {
+        this.gameId = this.route.snapshot.paramMap.get('id');
+
+        if (this.gameId)
+        {
+            this.auth.user$.pipe(
+                filter((user): user is { sub: string } => user != null),
+                switchMap(user => this.loadGameDetails(this.gameId || '', user.sub))
+            ).subscribe(
+                (data: Game) =>
+                {
+                    this.game = data;
+                    console.log('Game details:', this.game);
+                },
+                (error) =>
+                {
+                    console.error('Error fetching game details:', error);
+                }
+            );
+        }
+    }
+
+    loadGameDetails(gameId: string, sub: string): Observable<Game>
+    {
+        return this.gameService.getGameById(gameId, sub);
+    }
   
-  shareGame(pgn : string): void 
-  {
-    this.clipboard.copy(pgn);
-    console.log('Game URL copied to clipboard:', pgn);
-  }
+    shareGame(pgn: string): void
+    {
+        this.clipboard.copy(pgn);
+        console.log('Game PGN copied to clipboard:', pgn);
+        this.showCopiedMessage();
+    }
 
-  goBack(): void 
-  {
-    this.router.navigate(['../'], { relativeTo: this.router.routerState.root });
-  }
+    goBack(): void
+    {
+        this.router.navigate(['../'], { relativeTo: this.router.routerState.root });
+    }
+
+    private showCopiedMessage(): void
+    {
+        if (this.overlayRef)
+        {
+            this.overlayRef.dispose();
+        }
+
+        this.overlayRef = this.overlay.create(
+        {
+            positionStrategy: this.overlay.position()
+                .global()
+                .bottom('20px')
+                .right('20px'),
+            hasBackdrop: false
+        });
+
+        const messagePortal = new TemplatePortal(this.copiedMessage, this.viewContainerRef);
+        this.overlayRef.attach(messagePortal);
+
+        setTimeout(() =>
+        {
+            if (this.overlayRef)
+            {
+                this.overlayRef.dispose();
+                this.overlayRef = null;
+            }
+        }, 3000);
+    }
 }

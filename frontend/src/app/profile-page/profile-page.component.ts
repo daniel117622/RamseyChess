77eadfe96a5f5f9441d997bc986aca5ec3c3468e
daniel117622 <dan5jul@hotmail.com>
2025-04-04 14:37:05 -0700

    date display

--- frontend/src/app/profile-page/profile-page.component.ts
+++ frontend/src/app/profile-page/profile-page.component.ts
@@ -166,6 +166,18 @@ export class ProfilePageComponent implements OnInit {
     return
   }
 
+  convertUTCToLocal (utcDate: string): string 
+  {
+    const date = new Date(utcDate);
+  
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day   = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const mins  = String(date.getMinutes()).padStart(2, '0');
+  
+    return `${month} / ${day} ${hours}:${mins}`;
+  }
+
   fetchGames(itemsPerPage: number, pageNumber: number): void
   {
       this.user$.pipe(
